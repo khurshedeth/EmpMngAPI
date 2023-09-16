cffe761c4593d0f465dd054ab043f0ecea5d0a74
khurshedeth <khurshed.eth@gmail.com>
2023-09-17 02:44:37 +0530

    putRequest

--- database/Controller.js
+++ database/Controller.js
@@ -17,11 +17,26 @@ export async function postUser(req,res){
     const formData=req.body;
     if(!formData)return res.status(404).json({error:"Form Data not provided"});
     Users.create(formData,function(err,data){
-        return res.status(200).jason(data)
+        return res.status(200).json(data)
     })
 
     
     }catch(error){
         return res.status(404).json({error})
     }
+}
+
+// Put Request //
+export async function putUser(req,res){
+try{
+const{userId}= req.query;
+const formData=req.body;
+if(userId && formData){
+    await Users.findByIdAndUpdate(userId,formData);
+    res.status(200).json(formData)
+}
+res.status(404).json({error:"user not selected"})
+}catch(error){
+    res.status(404).json({error:"error while updating the data"})
+}
 }
