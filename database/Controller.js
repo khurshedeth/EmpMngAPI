a89a37160da8e7240e80cf46a2abe8950bd28ff9
khurshedeth <khurshed.eth@gmail.com>
2023-09-18 03:22:10 +0530

    minor changes

--- database/Controller.js
+++ database/Controller.js
@@ -12,6 +12,20 @@ export async function getUser(req, res) {
   }
 }
 
+//http://localhost:3000/api/hello/1
+
+export async function getUser(req,res){
+  try{
+const {userId}=req.query
+ if(userId){
+  const user=Users.findById(userId)
+  res.status(200).json(user)
+ }
+ res.status(404).json({error:"User not selected"})
+  } catch(error){
+    res.status(404).json({error:"Can not get the user"})
+  }
+}
 
 // Post Request//
 export async function postUser(req,res){
--- pages/api/User/[userId].js
+++ pages/api/User/[userId].js
@@ -1,15 +1,25 @@
 import connectMongo from "../../../database/conn";
-import {
-  getUser,
-  postUser,
-  putUser,
-  deleteUser,
-} from "../../../database/Controller";
+import { getUser, putUser, deleteUser } from "../../../database/Controller";
 
 export default async function handler(req, res) {
- const {method}=req
+  connectMongo().catch(() =>
+    res.status(405).json({ error: "error in the component" })
+  );
 
- switch(method){
-    case "Get":
- }
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      getUser(req, res);
+      break;
+    case "PUT":
+      putUser(req, res);
+      break;
+    case "DELETE":
+      deleteUser(req, res);
+
+    default:
+      res.setHeader("ALLOW", ["GET", "POST", "PUT", "DELETE"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
 }
